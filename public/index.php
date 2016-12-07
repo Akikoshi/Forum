3cbc829964cbdb05af34dcbdd02060ba842389b8
Akikoshi <Theise80@gmail.com>
2016-12-07 18:56:02 +0100

    create structure of Project

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 07.12.2016
+ * Time: 18:40
+ */
+namespace Semjasa\Heise;
+require_once (__DIR__ . '/../vendor/autoload.php');
+new Init();
---
+++ src/Init.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 07.12.2016
+ * Time: 18:40
+ */
+
+namespace Semjasa\Heise;
+
+
+class Init
+{  
+    public function __construct()
+    {
+        echo "Hallo Welt!";
+    }
+}
